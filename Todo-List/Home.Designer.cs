8cb0dc645ba04fc8f7ab6dc33d6aa2cf322ff4ba
Enterprise09 <goldenstaytes@gmail.com>
2021-11-27 23:15:26 +0900

    [+] import icon file

--- Todo-List/Home.Designer.cs
+++ Todo-List/Home.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Todo_List
 {
-    partial class Form1
+    partial class Home
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,10 +29,19 @@ namespace Todo_List
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.SuspendLayout();
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Home";
+            this.Text = "DASHBOARD";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
