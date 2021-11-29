7b6417f72b053cd5fff2649caad5a141c2bc9979
Enterprise09 <goldenstaytes@naver.com>
2021-11-30 00:48:12 +0900

    basic navigation bar design

--- Todo-List/Home.Designer.cs
+++ Todo-List/Home.Designer.cs
@@ -30,21 +30,240 @@ namespace Todo_List
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_logout = new System.Windows.Forms.Label();
+            this.lbl_setting = new System.Windows.Forms.Label();
+            this.lbl_notepad = new System.Windows.Forms.Label();
+            this.lbl_addjob = new System.Windows.Forms.Label();
+            this.lbl_joblist = new System.Windows.Forms.Label();
+            this.lbl_dashboard = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.lbl_logout);
+            this.panel1.Controls.Add(this.lbl_setting);
+            this.panel1.Controls.Add(this.lbl_notepad);
+            this.panel1.Controls.Add(this.lbl_addjob);
+            this.panel1.Controls.Add(this.lbl_joblist);
+            this.panel1.Controls.Add(this.lbl_dashboard);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(444, 1041);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel3.Location = new System.Drawing.Point(0, 333);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(12, 94);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.pictureBox5);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(444, 333);
+            this.panel2.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(146, 251);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 22);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "user@gmail.com";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(163, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 41);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "NAME";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(147, 31);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox5.TabIndex = 7;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(325, 350);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(61, 61);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(361, 973);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(361, 910);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Todo_List.Properties.Resources.clipboard;
+            this.pictureBox1.Location = new System.Drawing.Point(325, 436);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(61, 61);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_logout
+            // 
+            this.lbl_logout.AutoSize = true;
+            this.lbl_logout.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_logout.Location = new System.Drawing.Point(191, 977);
+            this.lbl_logout.Name = "lbl_logout";
+            this.lbl_logout.Size = new System.Drawing.Size(164, 41);
+            this.lbl_logout.TabIndex = 5;
+            this.lbl_logout.Text = "LOGOUT";
+            // 
+            // lbl_setting
+            // 
+            this.lbl_setting.AutoSize = true;
+            this.lbl_setting.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_setting.Location = new System.Drawing.Point(191, 914);
+            this.lbl_setting.Name = "lbl_setting";
+            this.lbl_setting.Size = new System.Drawing.Size(167, 41);
+            this.lbl_setting.TabIndex = 4;
+            this.lbl_setting.Text = "SETTING";
+            // 
+            // lbl_notepad
+            // 
+            this.lbl_notepad.AutoSize = true;
+            this.lbl_notepad.Font = new System.Drawing.Font("Arial", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_notepad.Location = new System.Drawing.Point(18, 613);
+            this.lbl_notepad.Name = "lbl_notepad";
+            this.lbl_notepad.Size = new System.Drawing.Size(230, 51);
+            this.lbl_notepad.TabIndex = 3;
+            this.lbl_notepad.Text = "NOTEPAD";
+            // 
+            // lbl_addjob
+            // 
+            this.lbl_addjob.AutoSize = true;
+            this.lbl_addjob.Font = new System.Drawing.Font("Arial", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_addjob.Location = new System.Drawing.Point(18, 527);
+            this.lbl_addjob.Name = "lbl_addjob";
+            this.lbl_addjob.Size = new System.Drawing.Size(215, 51);
+            this.lbl_addjob.TabIndex = 2;
+            this.lbl_addjob.Text = "ADD JOB";
+            // 
+            // lbl_joblist
+            // 
+            this.lbl_joblist.AutoSize = true;
+            this.lbl_joblist.Font = new System.Drawing.Font("Arial", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_joblist.Location = new System.Drawing.Point(18, 441);
+            this.lbl_joblist.Name = "lbl_joblist";
+            this.lbl_joblist.Size = new System.Drawing.Size(204, 51);
+            this.lbl_joblist.TabIndex = 1;
+            this.lbl_joblist.Text = "JOBLIST";
+            // 
+            // lbl_dashboard
+            // 
+            this.lbl_dashboard.AutoSize = true;
+            this.lbl_dashboard.Font = new System.Drawing.Font("Arial", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_dashboard.Location = new System.Drawing.Point(18, 355);
+            this.lbl_dashboard.Name = "lbl_dashboard";
+            this.lbl_dashboard.Size = new System.Drawing.Size(301, 51);
+            this.lbl_dashboard.TabIndex = 0;
+            this.lbl_dashboard.Text = "DASHBOARD";
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1804, 1041);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Home";
             this.Text = "DASHBOARD";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_logout;
+        private System.Windows.Forms.Label lbl_setting;
+        private System.Windows.Forms.Label lbl_notepad;
+        private System.Windows.Forms.Label lbl_addjob;
+        private System.Windows.Forms.Label lbl_joblist;
+        private System.Windows.Forms.Label lbl_dashboard;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
     }
 }
 
