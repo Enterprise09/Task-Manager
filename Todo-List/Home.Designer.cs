28303904ada307583a42cd8e254f9adc90fbb872
Enterprise09 <goldenstaytes@naver.com>
2021-11-30 12:49:04 +0900

    [+] nav menu icon & click event

--- Todo-List/Home.Designer.cs
+++ Todo-List/Home.Designer.cs
@@ -33,12 +33,15 @@ namespace Todo_List
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.lbl_logout = new System.Windows.Forms.Label();
             this.lbl_setting = new System.Windows.Forms.Label();
@@ -52,6 +55,8 @@ namespace Todo_List
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -63,6 +68,8 @@ namespace Todo_List
             this.panel1.Controls.Add(this.pictureBox2);
             this.panel1.Controls.Add(this.pictureBox4);
             this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox7);
+            this.panel1.Controls.Add(this.pictureBox6);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.lbl_logout);
             this.panel1.Controls.Add(this.lbl_setting);
@@ -73,7 +80,7 @@ namespace Todo_List
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(444, 1041);
+            this.panel1.Size = new System.Drawing.Size(444, 941);
             this.panel1.TabIndex = 0;
             // 
             // panel3
@@ -87,6 +94,7 @@ namespace Todo_List
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel2.Controls.Add(this.panel4);
             this.panel2.Controls.Add(this.label2);
             this.panel2.Controls.Add(this.label1);
             this.panel2.Controls.Add(this.pictureBox5);
@@ -95,6 +103,15 @@ namespace Todo_List
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(444, 333);
             this.panel2.TabIndex = 8;
+            // 
+            // panel4
+            // 
+            this.panel4.AutoScroll = true;
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel4.Location = new System.Drawing.Point(446, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 100);
+            this.panel4.TabIndex = 1;
             // 
             // label2
             // 
@@ -128,7 +145,7 @@ namespace Todo_List
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(325, 350);
+            this.pictureBox2.Location = new System.Drawing.Point(341, 350);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(61, 61);
             this.pictureBox2.TabIndex = 7;
@@ -137,7 +154,7 @@ namespace Todo_List
             // pictureBox4
             // 
             this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(361, 973);
+            this.pictureBox4.Location = new System.Drawing.Point(367, 880);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(48, 48);
             this.pictureBox4.TabIndex = 6;
@@ -146,16 +163,34 @@ namespace Todo_List
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(361, 910);
+            this.pictureBox3.Location = new System.Drawing.Point(367, 817);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(48, 48);
             this.pictureBox3.TabIndex = 6;
             this.pictureBox3.TabStop = false;
             // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+            this.pictureBox7.Location = new System.Drawing.Point(341, 608);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(61, 61);
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(341, 522);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(61, 61);
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Todo_List.Properties.Resources.clipboard;
-            this.pictureBox1.Location = new System.Drawing.Point(325, 436);
+            this.pictureBox1.Location = new System.Drawing.Point(341, 436);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(61, 61);
             this.pictureBox1.TabIndex = 6;
@@ -165,7 +200,7 @@ namespace Todo_List
             // 
             this.lbl_logout.AutoSize = true;
             this.lbl_logout.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lbl_logout.Location = new System.Drawing.Point(191, 977);
+            this.lbl_logout.Location = new System.Drawing.Point(197, 884);
             this.lbl_logout.Name = "lbl_logout";
             this.lbl_logout.Size = new System.Drawing.Size(164, 41);
             this.lbl_logout.TabIndex = 5;
@@ -175,7 +210,7 @@ namespace Todo_List
             // 
             this.lbl_setting.AutoSize = true;
             this.lbl_setting.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lbl_setting.Location = new System.Drawing.Point(191, 914);
+            this.lbl_setting.Location = new System.Drawing.Point(197, 821);
             this.lbl_setting.Name = "lbl_setting";
             this.lbl_setting.Size = new System.Drawing.Size(167, 41);
             this.lbl_setting.TabIndex = 4;
@@ -185,54 +220,61 @@ namespace Todo_List
             // 
             this.lbl_notepad.AutoSize = true;
             this.lbl_notepad.Font = new System.Drawing.Font("Arial", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lbl_notepad.Location = new System.Drawing.Point(18, 613);
+            this.lbl_notepad.Location = new System.Drawing.Point(34, 613);
             this.lbl_notepad.Name = "lbl_notepad";
             this.lbl_notepad.Size = new System.Drawing.Size(230, 51);
             this.lbl_notepad.TabIndex = 3;
             this.lbl_notepad.Text = "NOTEPAD";
+            this.lbl_notepad.Click += new System.EventHandler(this.lbl_notepad_Click);
             // 
             // lbl_addjob
             // 
             this.lbl_addjob.AutoSize = true;
             this.lbl_addjob.Font = new System.Drawing.Font("Arial", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lbl_addjob.Location = new System.Drawing.Point(18, 527);
+            this.lbl_addjob.Location = new System.Drawing.Point(34, 527);
             this.lbl_addjob.Name = "lbl_addjob";
             this.lbl_addjob.Size = new System.Drawing.Size(215, 51);
             this.lbl_addjob.TabIndex = 2;
             this.lbl_addjob.Text = "ADD JOB";
+            this.lbl_addjob.Click += new System.EventHandler(this.lbl_addjob_Click);
             // 
             // lbl_joblist
             // 
             this.lbl_joblist.AutoSize = true;
             this.lbl_joblist.Font = new System.Drawing.Font("Arial", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lbl_joblist.Location = new System.Drawing.Point(18, 441);
+            this.lbl_joblist.Location = new System.Drawing.Point(34, 441);
             this.lbl_joblist.Name = "lbl_joblist";
             this.lbl_joblist.Size = new System.Drawing.Size(204, 51);
             this.lbl_joblist.TabIndex = 1;
             this.lbl_joblist.Text = "JOBLIST";
+            this.lbl_joblist.Click += new System.EventHandler(this.lbl_joblist_Click);
             // 
             // lbl_dashboard
             // 
             this.lbl_dashboard.AutoSize = true;
             this.lbl_dashboard.Font = new System.Drawing.Font("Arial", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lbl_dashboard.Location = new System.Drawing.Point(18, 355);
+            this.lbl_dashboard.Location = new System.Drawing.Point(34, 355);
             this.lbl_dashboard.Name = "lbl_dashboard";
             this.lbl_dashboard.Size = new System.Drawing.Size(301, 51);
             this.lbl_dashboard.TabIndex = 0;
             this.lbl_dashboard.Text = "DASHBOARD";
+            this.lbl_dashboard.Click += new System.EventHandler(this.lbl_dashboard_Click);
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
+            this.AutoScroll = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(1804, 1041);
+            this.ClientSize = new System.Drawing.Size(1804, 941);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DASHBOARD";
+            this.Load += new System.EventHandler(this.Home_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
@@ -241,6 +283,8 @@ namespace Todo_List
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -264,6 +308,9 @@ namespace Todo_List
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Panel panel4;
     }
 }
 
--- Todo-List/Home.cs
+++ Todo-List/Home.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Todo_List
 {
@@ -17,5 +18,36 @@ namespace Todo_List
             InitializeComponent();
         }
 
+        private void Home_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void lbl_dashboard_Click(object sender, EventArgs e)
+        {
+            Form form = new Form();
+            form.Text = "Dashboard";
+            form.Show();
+        }
+
+        private void lbl_joblist_Click(object sender, EventArgs e)
+        {
+            Form form = new Form();
+            form.Text = "JobList";
+            form.Show();
+        }
+
+        private void lbl_addjob_Click(object sender, EventArgs e)
+        {
+            Form form = new Form();
+            form.Text = "AddJob";
+            form.Show();
+        }
+
+        private void lbl_notepad_Click(object sender, EventArgs e)
+        {
+            Form form = new Form();
+            form.Text = "Notepad";
+            form.Show();
+        }
     }
 }
